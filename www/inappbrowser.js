cfc9d45722c9fbfa90d539d51e120933431ae82e
Georgii Korshunov <gkorshunov@gog.com>
2017-09-03 16:33:50 +0200

    Catching errors accessing window.parent

--- www/inappbrowser.js
+++ www/inappbrowser.js
@@ -21,9 +21,13 @@
 
 (function () {
     // special patch to correctly work on Ripple emulator (CB-9760)
-    if (window.parent && !!window.parent.ripple) { // https://gist.github.com/triceam/4658021
-        module.exports = window.open.bind(window); // fallback to default window.open behaviour
-        return;
+    try {
+        if (window.parent && !!window.parent.ripple) { // https://gist.github.com/triceam/4658021
+            module.exports = window.open.bind(window); // fallback to default window.open behaviour
+            return;
+        }
+    } catch (e) {
+
     }
 
     var exec = require('cordova/exec');
